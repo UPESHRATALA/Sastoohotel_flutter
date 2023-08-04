61a6b24382848be57dae6b6916977bb434852fd4
sudip khati <sudipkhati2043@gmail.com>
2023-08-04 22:15:32 +0545

    room

--- lib/config/router/app_route.dart
+++ lib/config/router/app_route.dart
@@ -1,6 +1,8 @@
+import 'package:flutter/widgets.dart';
 import 'package:room_finder_app/fetures/auth/presentation/view/login.dart';
 import 'package:room_finder_app/fetures/auth/presentation/view/register.dart';
 import 'package:room_finder_app/fetures/home/presentation/view/dashboard.dart';
+import 'package:room_finder_app/fetures/room/domain/entity/room_entity.dart';
 import 'package:room_finder_app/fetures/room/presentation/view/add_room_view.dart';
 import 'package:room_finder_app/fetures/room/presentation/view/update_rooms.dart';
 import 'package:room_finder_app/fetures/room/presentation/view/upload_image_view.dart';
@@ -23,7 +25,10 @@ class AppRoute {
       singUpRoute: (context) => const Register(),
       dashboardRoute: (context) => const DashBoard(),
       uploadViews: (context) => const UploadView(),
-      updateRoom: (context) => const UpdateView(),
+      updateRoom: (context) {
+        final room = ModalRoute.of(context)!.settings.arguments as RoomEntity;
+        return UpdateView(room: room);
+      },
     };
   }
 }
